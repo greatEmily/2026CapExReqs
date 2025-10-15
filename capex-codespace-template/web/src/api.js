e77d561f245e24c6b0e0cc7d2a1149d86f671583
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2025-10-15 21:27:34 +0000

    starting over

--- capex-codespace-template/web/src/api.js
+++
@@ -1,8 +0,0 @@
-
-const API_URL = 'https://orange-carnival-xpj4gj4v7r7cpprg-3000.app.github.dev/'
-
-export async function getProjects() {
-  const res = await fetch(`${API_URL}/api/projects`);
-  if (!res.ok) throw new Error('Failed to fetch projects');
-  return res.json();
-}
