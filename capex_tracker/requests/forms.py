8da0ebf8e88df0a65673b9434af368d4295eebb4
Emily Duran <63298612+greatEmily@users.noreply.github.com>
2025-10-21 18:47:54 +0000

    testing forms. please save this time lol

--- capex_tracker/requests/forms.py
+++ capex_tracker/requests/forms.py
@@ -14,11 +14,27 @@ class RequestForm(forms.Form):
         queryset=Equipment.objects.all(),
         widget=forms.Select(attrs={'class': 'form-select'})
     )
-    type = forms.ModelChoiceField(
-        queryset=Type.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-select'})
+
+    TYPE_CHOICES = [
+        ('New Equipment', 'New Equipment'),
+        ('Replacement', 'Replacement')
+    ]
+
+    type = forms.ChoiceField(
+        choices=TYPE_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-select'}),
+        label='Request Type'
     )
+
     approval_status = forms.CharField(
         initial='Requested',
         widget=forms.HiddenInput()
     )
+    business_case = forms.CharField(
+        widget=forms.Textarea(attrs={
+            'class': 'form-control',
+            'placeholder': 'Describe business case here for the equipemnt requested.',
+            'rows': 4
+        }),
+        label='Business Case'
+    ) 
